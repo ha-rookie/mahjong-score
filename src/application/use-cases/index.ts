be7bb9dadf46983178c028674bb2ab5bbb2d273d
ha-rookie <104521513+ha-rookie@users.noreply.github.com>
2026-09-27 00:22:19 +0900

    Expose group rename use case

--- src/application/use-cases/index.ts
+++ src/application/use-cases/index.ts
@@ -2,6 +2,7 @@ export * from "./add-game-result";
 export * from "./add-player-to-group";
 export * from "./backup";
 export * from "./create-group";
+export * from "./update-group-name";
 export * from "./cancel-empty-session";
 export * from "./read-models";
 export * from "./start-session";
